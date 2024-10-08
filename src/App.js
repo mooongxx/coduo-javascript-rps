fc0c22a7f18ad3a3c2d769d756e7b812da84777d
anttiey <hanuuny@gmail.com>
2024-10-08 20:25:17 +0900

    refactor: 예시용 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Console from './utils/Console.js';
+
 class App {
   async play() {
-    console.log('Hello World!');
-    return 'Hello World!';
+    Console.print('가위바위보 게임을 시작합니다.');
+    const count = await Console.readLineAsync('승리를 위한 횟수를 입력하세요: ');
+
+    // 코드를 작성해 주세요.
+
+    return;
   }
 }
 
